783f571c7bceb40a2a40b6d9229ab4ac652eb7c5
“Salil48” <salilchharia@gmail.com>
2022-12-24 18:21:05 +0530

    Adding username

---
+++ Username.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Username
+    {
+        int a;
+        public static void DisplayUsername()
+        {
+            Console.WriteLine("Please enter the username :");
+
+            string username = Console.ReadLine();
+
+
+            if (username.Length > 3)
+            {
+
+                Console.WriteLine("Hello " + username + ", How are you ?");
+            }
+            else
+            {
+                Console.WriteLine("The username is not having 3 characters ");
+            }
+        }
+    }
+}
