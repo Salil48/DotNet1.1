8338ee23b69666f77d80710cc4dc04d5396a0324
“Salil48” <salilchharia@gmail.com>
2022-12-29 11:50:51 +0530

    Find Vowel and Consonant

---
+++ VowelorConsonant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class VowelorConsonant
+    {
+        public static void Check()
+        {
+            Console.WriteLine("Enter a Alphabet : ");
+            char ch = Convert.ToChar(Console.ReadLine().ToLower());
+
+            switch(ch) 
+            {
+                case 'a':
+                Console.WriteLine("The Alphabet is vowel");
+                break;
+                case 'e':
+                Console.WriteLine("The Alphabet is vowel");
+                break;
+                case 'i':
+                Console.WriteLine("The Alphabet is vowel");
+                break;
+                case 'o':
+                Console.WriteLine("The Alphabet is vowel");
+                break;
+                case 'u':
+                Console.WriteLine("The Alphabet is vowel");
+                break;
+                default :
+                Console.WriteLine("The Alphabet is not a vowel");
+                break;
+
+
+            }
+        }
+    }
+}
